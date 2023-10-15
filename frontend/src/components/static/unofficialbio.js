e84ad0128b57ee162d8e4dab883d27624c999053
Jim Chen <123833550+jimchen2@users.noreply.github.com>
2023-10-15 05:55:50 -0700

    Update unofficialbio.js

--- frontend/src/components/static/unofficialbio.js
+++ frontend/src/components/static/unofficialbio.js
@@ -13,70 +13,24 @@ function BIO() {
     >
       <br />
       <br />
-      <h3 id="identity">Identity</h3>
-      <p>
-        I am an individualist and I believe in freedom. I think from my own
-        perspective.
-      </p>
-      <p>
-        I am a minimalist, and I prefer simple things. In life I seldom get more
-        things than necessary, and I try to be simple and direct in both
-        speaking and writing.
-      </p>
-      <h3 id="preferences">Preferences</h3>
-      <p>
-        I am a devoted Linux user. I won&#39;t use Windows and mouse anymore.
-      </p>
-      <p>I have erratic sleep schedule.</p>
-      <p>
-        {/* I love traveling, and there are many places yet to go. I want to go to
-        North Pole, Russia, Norway, Iceland, Italy, Kenya, and Australia. */}
-      </p>
-      <h3 id="hobbies">Hobbies</h3>
-      <p>
-        I live on the Internet and I would go browse all kinds of stuff for
-        hours every day. I love watching YouTube, but I think Bilibili is more
-        interactive. Some contents I like include{" "}
-        <a href="https://www.youtube.com/@NNRunningTeam">Running</a>,{" "}
-        <a href="https://www.youtube.com/watch?v=EFuxhi45ZbM&t=58s&ab_channel=NBCSports">
-          Figure-skating
-        </a>
-        , Vlogs, <a href="https://www.youtube.com/@freecodecamp">Tutorials</a>,{" "}
-        <a href="https://smotrim.ru/live/21">News</a>,{" "}
-        <a href="https://www.youtube.com/@bluetractortv">Cartoons</a>,{" "}
-        <a href="https://www.youtube.com/@PatrikPietschmann">Music</a>,{" "}
-        <a href="https://www.youtube.com/watch?v=nImK2qsYoFM&ab_channel=FreeHigh-QualityDocumentaries">
-          Documentaries
-        </a>{" "}
-        and{" "}
-        <a href="https://www.youtube.com/watch?v=linlz7-Pnvw&t=638s&ab_channel=8KWorld">
-          Travel Guides
-        </a>
-        .{" "}
-      </p>
-      <p>
-        I also love watching{" "}
-        <a href="https://smotrim.ru/live/63250">movies and tvs</a>, but do not have
-        specific preferences.
-      </p>
-      <p>
-        I used to really enjoy reading, but in the last 5 years I haven&#39;t
-        read much.
-      </p>
-      <p>
-        I love running and I usually run at 4-5 min/kilometer pace at evenings.
-      </p>
-      <p>I love hiking and I usually go out in weekends.</p>
-      <h3 id="previous-websites">Previous Websites</h3>
-      <p>
-        <a href="https://jimchen4214.wixsite.com/12345">My previous website</a>{" "}
-        is build using <a href="https://wix.com">Wix</a>. Wix is easy and slow,
-        so I don&#39;t like Wix now.
-      </p>
-      <p>
-        {" "}
-        I also have a blog with <a href="thumb.jpeg">WeChat offical account</a>.
-      </p>
+<h2 id="identity">Identity</h2>
+<p>I am an individualist and libertarian. </p>
+<p>I am a minimalist. </p>
+<h2 id="preferences">Preferences</h2>
+<p>I use Arch btw. </p>
+<p><a href="https://www.jimchen.me/Wed%20Oct%2004%202023%2017:30:01">I prefer FOSS software</a>.</p>
+<p>I like using Gnome, Bash, LibreOffice, OSS Code, Chromium, DuckDuckGo, Tor, Invidious, Dropbox, MongoDB, Linode, ChatGPT. </p>
+<p>I am neutral in Instagram, Iphone, Reddit, WeChat, QQ.</p>
+<p>I don&#39;t use Windows, Mouse, MSOffice, VSCode, Google Chrome, Twitter, Discord, Baidu.</p>
+<p><a href="https://www.jimchen.me/Wed%20Oct%2004%202023%2018:07:34">My browser font is Courier New</a>.</p>
+<h2 id="hobbies">Hobbies</h2>
+<p>I like browsing Internet.</p>
+<p>I like <a href="https://www.instagram.com/ludovico_einaudi/">Einaudi</a>&#39;s music, including Experience, Nuvole Bianche, Primavera, etc.</p>
+<p>I like running and usually run at 4 min/kilometer pace at evenings.</p>
+<h2 id="previous-websites">Previous Websites</h2>
+<p><a href="https://jimchen4214.wixsite.com/12345/about-me">My previous website</a> is built on <a href="https://wix.com/">Wix</a>, but I dislike Wix.</p>
+<p>I also <a href="https://www.jimchen.me/thumb.jpeg">blog on Wechat</a>.</p>
+
       <br />
       <br />
       <br />
