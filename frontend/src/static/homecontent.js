20e491ac4abb4af756293feb6306c329e443a848
Jim Chen <123833550+jimchen2@users.noreply.github.com>
2024-04-17 04:41:05 +0800

    Update homecontent.js

--- frontend/src/static/homecontent.js
+++ frontend/src/static/homecontent.js
@@ -12,51 +12,49 @@ const htmlContent = {
 
 <h3 id="self-hosting"><strong>My Frontends</strong></h3>
 <ul>
-  <li><strong>Invidious Instance</strong>: Access YouTube content. <a href="https://nexttube.org">nexttube.org</a> <a href="https://invidious.jimchen.me">Alternative
+  <li><strong>Invidious Instance</strong>: <a href="https://nexttube.org">nexttube.org</a> <a href="https://invidious.jimchen.me">Alternative
   url</a></li>
   <li><strong>AnonyTube</strong>: AnonyTube is a beta-stage, free, ad-free video platform focusing on openness with automatic subtitles, video translations. <a href="https://tubeapp.org/">AnonyTube</a> <a href="https://anonytube.jimchen.me">Alternative
   url</a></li>
 </ul>
+
+
 <h3 id="wiki"><strong>Wiki</strong></h3>
 <ul>
   <li><strong>ArchLinux Wiki</strong>: <a href="https://wiki.archlinux.org/">wiki.archlinux.org</a></li>
   <li><strong>Gentoo Wiki</strong>: <a href="https://wiki.gentoo.org/">wiki.gentoo.org</a></li>
-  <li><strong>LFS docs</strong>: <a href="https://www.linuxfromscratch.org/lfs/read.html">linuxfromscratch.org/lfs/read.html</a></li>
+  <li><strong>Linux From Scratch</strong>: <a href="https://www.linuxfromscratch.org/lfs/read.html">linuxfromscratch.org/lfs/read.html</a></li>
+  <li><strong>LineageOS Wiki</strong>: <a href="https://wiki.lineageos.org/">wiki.lineageos.org</a></li>
+  <li><strong>PostmarketOS Wiki</strong><a href="https://wiki.postmarketos.org/">wiki.postmarketos.org</a></li>
+   <li><strong>Linux Kernel documentation</strong><a href="https://docs.kernel.org/">docs.kernel.org</a></li>
   <li><strong>FreeBSD Wiki</strong>: <a href="https://wiki.freebsd.org/">wiki.freebsd.org</a></li>      
-  <li><strong>Cloudflare Docs</strong>: <a href="https://developers.cloudflare.com/pages">developers.cloudflare.com/pages</a></li>
-  <li><strong>Linode Docs</strong>: <a href="https://www.linode.com/docs/">linode.com/docs</a></li>
-  <li><strong>The Hidden Wiki</strong>: <a href="https://thehiddenwiki.org/">thehiddenwiki.org</a></li>
+  <li><strong>Cloudflare Docs</strong>: <a href="https://developers.cloudflare.com">developers.cloudflare.com</a></li>
   <li><strong>PostgreSQL</strong>: <a href="https://www.postgresql.org/docs/current/">postgresql.org/docs/current</a></li>
   <li><strong>MongoDB</strong>: <a href="https://www.mongodb.com/docs/">mongodb.com/docs</a></li>
 </ul>
-<h3 id="pricing-information"><strong>Pricing Information</strong></h3>
+
+
+<h3 id="computer-networking"><strong>Networking</strong></h3>
 <ul>
-  <li><strong>Free Resources for Developement</strong>: <a href="https://github.com/ripienaar/free-for-dev">github.com/ripienaar/free-for-dev</a></li>
-  <li><strong>GPU prices</strong>: <a href="https://cloud-gpus.com/">cloud-gpus.com</a></li>
-</ul>
-<h3 id="computer-networking"><strong>Computer Networking</strong></h3>
-<ul>
-  <li><strong>VPN providers</strong>: <a href="https://guatizi.com/">Provider Lists</a>, <a href="https://github.com/hwanz/SSR-V2ray-Trojan-vpn">Cheap VPN</a>
-  </li>
-    <li><strong>Clash</strong>: <a href="https://clash.wiki/">Clash</a></li>
-  <li><strong>Cloudflare Warp</strong>: Cloudflare’s performance and
-  security-focused VPN. <a href="https://1.1.1.1/">1.1.1.1</a></li>
+  <li><strong>VPN providers</strong>: <a href="https://guatizi.com/">Provider Lists</a>, <a href="https://github.com/hwanz/SSR-V2ray-Trojan-vpn">Cheap VPN</a> </li>
+  <li><strong>Cloudflare Warp</strong>: Cloudflare’s performance and security-focused VPN. <a href="https://1.1.1.1/">1.1.1.1</a></li>
   <li><strong>Tor Project</strong>: Browsing anonymously and dark web. <a href="https://www.torproject.org/">torproject.org</a></li>
   <li><strong>I2P</strong>: Invisible Internet Project: browsing the I2P
   anonymous network. <a href="https://geti2p.net/">geti2p.net</a></li>
-  <li><strong>Squid</strong>: Caching proxy to improve performances. <a href="https://www.squid-cache.org/">squid-cache.org</a></li>
-  <li><strong>PiHole</strong>: Network-wide ad blocker. <a href="https://pi-hole.net/">pi-hole.net</a></li>
 </ul>
+
+
 <h3 id="media"><strong>Self-Hosting</strong></h3>
 <ul>
   <li><strong>Invidious</strong>: Access YouTube content privately through hosted instances. <a href="https://invidio.us/">invidio.us</a></li>
-  <li><strong>FreeTube</strong>: Access YouTube content privately (App). <a href="https://github.com/FreeTubeApp/FreeTube">FreeTube on GitHub</a></li>
   <li><strong>PeerTube</strong>: Decentralized video hosting platform. <a href="https://github.com/Chocobozzz/PeerTube">PeerTube on GitHub</a></li>
   <li><strong>Mastodon</strong>: Decentralized social network. <a href="https://github.com/mastodon/mastodon">Mastodon on GitHub</a></li>
   <li><strong>Discourse</strong>: Open-source forum. <a href="https://github.com/discourse/discourse">Discourse on GitHub</a></li>
     <li><strong>Element</strong>: Secure Messaging. <a href="https://github.com/element-hq/element-web/">Element on GitHub</a></li>
       <li><strong>Libera Chat</strong>: Community Platform for FOSS projects. <a href="https://github.com/Libera-Chat/libera-chat.github.io">Libera Chat on GitHub</a></li>
 </ul>
+
+
 <h3 id="multipurpose-software"><strong>Multi-purpose software</strong></h3>
 <ul>
   <li><strong>FFmpeg</strong>: Audio/Video processing. <a href="https://github.com/FFmpeg/FFmpeg">FFmpeg on GitHub</a></li>
@@ -65,28 +63,21 @@ const htmlContent = {
   <li><strong>Selenium</strong>: Web Scraping. <a href="https://github.com/SeleniumHQ/selenium">Selenium on GitHub</a></li>
   <li><strong>curl</strong>: Web Requests. <a href="https://github.com/curl/curl">Curl on Github</a></li>
 </ul>
-<h3 id="laptop-purchase"><strong>Laptop Purchase</strong></h3>
+
+
+<h3 id="laptop-purchase"><strong>Laptop & Smartphone Purchase</strong></h3>
 <ul>
-  <li><strong>System76</strong>: Powerful Linux laptops. <a href="https://system76.com/laptops">System76</a></li>
-  <li><strong>Purism</strong>: Privacy and security focused Linux laptops. <a href="https://shop.puri.sm/shop/librem-14/">Librem 14</a></li>
-  <li><strong>Framework</strong>: DIY edition build laptop yourself. <a href="https://frame.work/marketplace/laptops">Framework Marketplace</a></li>
-  <li><strong>Juno Computers</strong>: Variety of Linux laptops. <a href="https://junocomputers.com/product-category/laptops/">Juno Computers</a></li>
-  <li><strong>PineBook</strong>: Cheap ARM laptop. <a href="https://pine64.org/devices/#laptops/">PineBook</a></li>
+  <li><strong>System76</strong>:  Linux Laptops, Desktops, and Servers. <a href="https://system76.com/laptops">system76.com/laptops</a></li>
+  <li><strong>Purism</strong>: Beautiful, Secure, Privacy-Respecting Devices. <a href="https://shop.puri.sm/shop/">shop.puri.sm/shop</a></li>
+  <li><strong>Framework</strong>: Fully Custom Thin, Powerful Notebook. <a href="https://frame.work/marketplace">frame.work/marketplace</a></li>
+  <li><strong>Pine64</strong>: High-quality, Low-cost ARM and RISC-V Devices. <a href="https://pine64.org/devices/">pine64.org/devices</a></li>
+/a></li>
 </ul>
-<h3 id="smartphone"><strong>Smartphone</strong></h3>
+
+<h3 id="mobile-operating-systems"><strong>Pricing</strong></h3>
 <ul>
-  <li><strong>PinePhone</strong>: Cheap phone supporting over 20 operating
-  systems. <a href="https://pine64.com/product-category/smartphones/">PinePhone</a></li>
-  <li><strong>Purism</strong>: Privacy and security focused Linux phones.
-  <a href="https://shop.puri.sm/shop/liberty-phone/">Liberty Phone</a>, <a href="https://shop.puri.sm/shop/librem-11/">Librem 11</a></li>
-  <li><strong>Murena</strong>: De-Googled /e/ os smartphones. <a href="https://murena.com/products/smartphones/">Murena/Fairphone</a></li>
-</ul>
-<h3 id="mobile-operating-systems"><strong>Mobile Operating Systems</strong></h3>
-<ul>
-  <li><strong>LineageOS</strong>: Open-source Android with no bloatwares, supporting many models. <a href="https://wiki.lineageos.org/devices/">LineageOS Devices</a></li>
-  <li><strong>/e/ OS</strong>: De-Googled fork of LineageOS. <a href="https://doc.e.foundation/devices">e Foundation Devices</a></li>
-  <li><strong>PostmarketOS</strong>: Alpine Linux for smartphones. <a href="https://wiki.postmarketos.org/wiki/Devices">PostmarketOS Devices</a></li>
-  <li><strong>ArchLinux ARM</strong>: <a href="https://archlinuxarm.org/platforms">ArchLinux ARM Platforms</a></li>
+  <li><strong>Free Resources for Developement</strong>: <a href="https://github.com/ripienaar/free-for-dev">github.com/ripienaar/free-for-dev</a></li>
+  <li><strong>GPU Pricing</strong>: <a href="https://cloud-gpus.com/">cloud-gpus.com</a></li>
 </ul>
     `,
 };
